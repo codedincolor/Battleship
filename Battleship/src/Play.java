8d6ecf5554def61ed51fb0faa2cafbcc3a10a2f7
JESSICA <jsobrique@yahoo.com>
2016-03-26 13:02:19 -0700

    Added amount of tries

--- Battleship/src/Play.java
+++ Battleship/src/Play.java
@@ -9,16 +9,18 @@ public class Play {
 		ship1.setLocation(4);
 		
 		boolean shipFound = false;
+		int tries = 0;
 		
 		while(shipFound==false)
 		{
+			tries++;
 			System.out.print("Guess a location: ");
 		    Scanner sc = new Scanner(System.in);
 		    int guess = sc.nextInt();
 			if(ship1.getLocation(guess)==true)
 			{
 				shipFound = true;
-				System.out.println("You found the ship!");
+				System.out.println("You found the ship in " + tries + " tries");
 			}	
 			else 
 			{
